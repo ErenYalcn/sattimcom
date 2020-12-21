97ff3596c9acb8c435ec88f3d6edb3774285e43e
Eren Yalçın <57539683+ErenYalcn@users.noreply.github.com>
2020-12-21 21:47:18 +0300

    Google Firebase ile üyelik oluşturma ve giriş yapma özellikleri eklendi. Ekstra çıkış yapabilmek için menu oluşturdum. UploadActivity dosyası boş.

---
+++ app/src/main/java/com/example/sattimcom/FeedActivity.java
@@ -0,0 +1,56 @@
+package com.example.sattimcom;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+public class FeedActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.insta_options_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.add_post) {
+            Intent intentToUpload = new Intent(FeedActivity.this, UploadActivity.class);
+            startActivity(intentToUpload);
+        } else if (item.getItemId() == R.id.signout) {
+
+            firebaseAuth.signOut();
+
+            Intent intentToSignUp = new Intent(FeedActivity.this, SignUpActivity.class);
+            startActivity(intentToSignUp);
+            finish();
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+}
